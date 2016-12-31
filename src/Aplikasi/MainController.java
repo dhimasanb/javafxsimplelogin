75dcad35c252fae350c6695ac1f3cefdbc1f1002
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2016-12-31 13:35:45 +0700

    integrasi code ke layout login

--- src/Aplikasi/MainController.java
+++ src/Aplikasi/MainController.java
@@ -7,7 +7,12 @@ package Aplikasi;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -22,6 +27,23 @@ public class MainController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+
+    @FXML
+    private Label lblStatus;
+    
+    @FXML
+    private TextField txtUsername;
+    
+    @FXML
+    private PasswordField txtPassword;
+    
+    public void Login (ActionEvent event){
+        if (txtUsername.getText().equals("user") && txtPassword.getText().equals("pass")) {
+            lblStatus.setText("Login berhasil");
+        } else {
+            lblStatus.setText("Login gagal");
+        }
+    }
     
 }
