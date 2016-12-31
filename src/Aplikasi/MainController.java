f24d5b4e525c9a714bccd53ae20e414465a02b10
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2016-12-31 13:45:19 +0700

    update halaman setelah login berhasil

--- src/Aplikasi/MainController.java
+++ src/Aplikasi/MainController.java
@@ -5,14 +5,19 @@
  */
 package Aplikasi;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -38,9 +43,14 @@ public class MainController implements Initializable {
     @FXML
     private PasswordField txtPassword;
     
-    public void Login (ActionEvent event){
+    public void Login (ActionEvent event) throws IOException{
         if (txtUsername.getText().equals("user") && txtPassword.getText().equals("pass")) {
             lblStatus.setText("Login berhasil");
+            Stage primaryStage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("/Aplikasi/Main.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
         } else {
             lblStatus.setText("Login gagal");
         }
